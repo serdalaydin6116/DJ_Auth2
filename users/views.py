70cf6140a0d46ce71bba981a500ae9ad227803d4
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-01 23:06:57 +0300

    synched with models and forms.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from multiprocessing import AuthenticationError
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserProfileForm, UserForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -17,8 +20,33 @@ def user_logout(request):
 def register(request):
     form_user=UserForm()
     form_profile=UserProfileForm()
+    if request.method=='POST':
+        form_user=UserForm(request.POST)
+        form_profile=UserProfileForm(request.POST, request.FILES)
+        if form_user.is_valid() and form_profile.is_valid():
+            # form_user.save()
+            user = form_user.save()
+            # form_profile.save() ilk form save edilince olusturulan ıd yi almam lazım
+            profile=form_profile.save(commit=False)
+            profile.user=user
+            profile.save()
+
+            login(request, user)
+
+            return redirect('home')
     context = {
         'form_profile': form_profile,
         'form_user': form_user
     }
     return render(request, 'users/register.html', context)
+
+
+def user_login(request):
+    form=AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+        user=form.get_user()
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'users/user_login.html', {'form':form})
+        
