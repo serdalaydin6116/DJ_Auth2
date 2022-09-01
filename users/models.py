0288a66dc8cf30142be5f5a2fb15a1820dec2b49
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-01 23:06:21 +0300

    adding new fields by using Abstract User

--- users/models.py
+++ users/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    portfolio = models.URLField(blank=True)
+# # Create your models here.
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     portfolio = models.URLField(blank=True)
+#     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+#     def __str__(self):
+#         return self.user.username
+
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    portfolio=models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
-    def __str__(self):
-        return self.user.username
+
