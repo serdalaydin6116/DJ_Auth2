fb98fb9b0712cccd8bc6050de3bcafe994da6b6d
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-01 22:02:37 +0300

    views and urls were synched with forms.py

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import user_logout, register
 
 urlpatterns = [
+    path('logout/', user_logout, name='user_logout'),
+    path('register/', register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
+from django.contrib.auth import logout
+from .forms import UserProfileForm, UserForm
+
 
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+
+def register(request):
+    form_user=UserForm()
+    form_profile=UserProfileForm()
+    context = {
+        'form_profile': form_profile,
+        'form_user': form_user
+    }
+    return render(request, 'users/register.html', context)
